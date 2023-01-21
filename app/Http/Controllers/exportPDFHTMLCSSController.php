82eef391f48da22e6f27f88391a9a3de8d848af0
elmasry2 <mohamadelmasry2@gmail.com>
2023-01-21 13:11:00 +0200

    [DEV]: add comment to explain

--- app/Http/Controllers/exportPDFHTMLCSSController.php
+++ app/Http/Controllers/exportPDFHTMLCSSController.php
@@ -14,8 +14,9 @@ class exportPDFHTMLCSSController extends Controller
         $data['left_image'] = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQytZbi7kITvTiF3O6mhRumcP5qD3p7kulmAQ&usqp=CAU';
         $data['right_image'] = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQytZbi7kITvTiF3O6mhRumcP5qD3p7kulmAQ&usqp=CAU';
         $data['users']=null;
+        // passing data to blade
         $html = view('pdfHTMLCSS',compact('data'))->render();
-
+            // load rendered data from pdf HTML View file and generate PDF fiel 
         $pdf = PDF::loadHTML($html);
         return $pdf
             ->setPaper('a4', 'portrait')
