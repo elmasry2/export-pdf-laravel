2ea7bde16c5e9b925e39c03dd00f12937ad4e27e
elmasry2 <mohamadelmasry2@gmail.com>
2023-01-21 14:50:53 +0200

    [DEV]: change watermark into text

--- app/Http/Controllers/exportPDFHTMLCSSController.php
+++ app/Http/Controllers/exportPDFHTMLCSSController.php
@@ -15,14 +15,14 @@ class exportPDFHTMLCSSController extends Controller
         $data['right_image'] = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQytZbi7kITvTiF3O6mhRumcP5qD3p7kulmAQ&usqp=CAU';
         $data['users']=null;
         // passing data to blade
-        $html = view('pdfHTMLCSS',compact('data'))->render();
-            // load rendered data from pdf HTML View file and generate PDF fiel 
-        $pdf = PDF::loadHTML($html);
-        return $pdf
-            ->setPaper('a4', 'portrait')
-            ->setOption(['dpi' => 150, 'defaultFont' => 'sans-serif'])
-            ->setWarnings(false)
-            ->download('invoice.pdf');
+         return $html = view('pdfHTMLCSS',compact('data'))->render();
+            // load rendered data from pdf HTML View file and generate PDF fiel
+//        $pdf = PDF::loadHTML($html);
+//        return $pdf
+////            ->setPaper('a4', 'portrait')
+//            ->setOption(['dpi' => 150, 'defaultFont' => 'sans-serif'])
+//            ->setWarnings(false)
+//            ->download('invoice.pdf');
 //        $pdf = PDF::loadView('invoice-pdf2');
 //        return $pdf->download('invoice-pdf2.pdf');
 
--- resources/views/pdfHTMLCSS.blade.php
+++ resources/views/pdfHTMLCSS.blade.php
@@ -1,4 +1,3 @@
-{{--{{dd('')}}--}}
     <!DOCTYPE html>
 <html>
 
@@ -8,27 +7,44 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$data['title']??''}} </title>
-{{--    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" />--}}
-    <link rel="stylesheet" href="{{asset('css/pdf.css')}}">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" />
+{{--    <link rel="stylesheet" href="{{asset('css/pdf.css')}}">--}}
 
 
 
     {{--    <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">--}}
 
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;500;900&family=Poppins:wght@300;400;500;600;700;800&family=Roboto:wght@300;400;500;700;900&family=Saira:wght@300;400;500;600;700;800;900&display=swap');
-
-        .report-main-container {
+    <style >
+        *{
             -webkit-print-color-adjust: exact !important;
-            background: rgb(2,0,36);
-            background: linear-gradient(0deg, rgb(207, 162, 166) 0%, rgb(187, 212, 186) 46%);
-            background-opacity:0.5;
-            background-size: 100%;
+            print-color-adjust: exact !important;
 
         }
+        /** {*/
+        /*    !*font-family: "Times New Roman";*!*/
+        /*    font-family: DejaVu Sans, sans-serif , arial , tahoma !important;*/
+
+        /*    font-weight: bolder !important;*/
+        /*}*/
+            /*font-family: DejaVu Sans, sans-serif , arial , tahoma !important; */
+        /*}*/
+
+        /*@import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;500;900&family=Poppins:wght@300;400;500;600;700;800&family=Roboto:wght@300;400;500;700;900&family=Saira:wght@300;400;500;600;700;800;900&display=swap');*/
+
+        /*.report-main-container {*/
+        /*    -webkit-print-color-adjust: exact !important;*/
+        /*    background: rgb(2,0,36);*/
+        /*    background: linear-gradient(0deg, rgb(207, 162, 166) 0%, rgb(187, 212, 186) 46%);*/
+        /*    background-opacity:0.5;*/
+        /*    background-size: 100%;*/
+
+        /*}*/
         table{
             width:100% !important;
             text-align:center !important;
+        }
+        table:nth-child(1){
+            /*page-break-after: always !important;*/
         }
         .main-title{
             font-size: 46px;
@@ -45,23 +61,56 @@
         }
 
         .watermark-image{
-            position: absolute !important;
+            /*position: absolute !important;*/
             opacity: .3 !important;
             top:40% !important;
             left:35% !important;
+            position:absolute;
+            z-index:-1;
+            display:block;
+            /*top: 50%;*/
+            /*left: 50%;*/
+            margin-top: -50px;
+            margin-left: -50px;
         }
-
-        body:before{
-            background-repeat:repeat;
-            background-image: url({{$data['small_watermark']}});
+        body {
+            min-height: 300vh;
+            position: relative;
+            margin: 0;
+        }
+        body:before {
             content: "";
-            background-size: 50px !important;
             position: absolute;
-            top: 0px;
-            right: 0px;
-            bottom: 0px;
-            left: 0px;
-            opacity: 0.2;
+            z-index: 9999;
+            inset: 0;
+            background:
+                url('data:image/svg+xml;utf8,<svg style="transform:rotate(0deg)" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 60 120"><text x="0" y="25" fill="gray"> NA-120 WaterMark </text></svg>')
+                0 0/100% 100vh;
+        }
+{{--                background-repeat:no-repeat;--}}
+{{--background-image: url({{$data['small_watermark']}});--}}
+{{--    background-size: 400px !important;--}}
+{{--            background-position: center;--}}
+
+}
+        @media  print
+        {
+            body {
+                min-height: 300vh;
+                position: relative;
+                margin: 0;
+            }
+            body:before {
+                content: "";
+                position: absolute;
+                z-index: 9999;
+                inset: 0;
+                background:
+                    url('data:image/svg+xml;utf8,<svg style="transform:rotate(0deg)" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 60 120"><text x="0" y="25" fill="gray"> NA-120 WaterMark </text></svg>')
+                    0 0/100% 100vh;
+            }
+            @page  { margin: 4px; }
+            @page{
         }
 
     </style>
@@ -74,17 +123,17 @@
     <div class="container-fluid report-container">
         <div class="container report-body">
             <table>
-                <tbody>
+                <thead>
                 <tr>
                     <th colspan="2"><img class="person-image" src="{{$data['left_image']??''}}"> </th>
                     <th colspan="4" class="main-title">{{$data['main_title']??''}}</th>
                     <th colspan="2"><img class="person-image" src="{{$data['right_image']??''}}"> </th>
                 </tr>
-                </tbody>
+                </thead>
             </table>
-            <div class="watermark-image " >
-                <img src="{{$data['center_watermark']}}" width="400" height="400" alt="">
-            </div>
+{{--            <div class="watermark-image " >--}}
+{{--                <img src="{{$data['center_watermark']}}" width="400" height="400" alt="">--}}
+{{--            </div>--}}
             <table class="main-table">
                 <thead>
 
@@ -101,7 +150,7 @@
                 </tr>
                 </thead>
                 <tbody>
-                @for($i=306;$i < 321; $i++)
+                @for($i=306;$i < 341; $i++)
                     <tr>
                         <td class="p-2"> 0317 4050 123 0317 4050 123 0317 4050 123 </td>
                         <td> سال مكان نمبر 21 - كلي نمبر 24 - محله احامله رحيم يخشي بزاني روك كزهي شابوه ، لابور  </td>
